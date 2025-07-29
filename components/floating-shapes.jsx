69b159f43d14ad28d7e3f0cfb7770b6f60f494f6
sharique ahmad <ahmedsharique250@gmail.com>
2025-07-29 14:24:41 +0530

    initiate setup

---
+++ components/floating-shapes.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const FloatingShapes = () => {
+  const shapes=[
+    {
+      id:1,
+      size:"w-72 h-72",
+      position:"top-20 left-10",
+      gradient:"from-blue-500  to-purple-600"
+    },
+    {
+     id:2,
+      size:"w-96 h-96",
+      position:"top-1/3 right-10",
+      gradient:"from-cyan-500  to-blue-600"
+    },
+    {
+      id:3,
+      size:"w-64 h-64",
+      position:"bottom-20 left-1/4",
+      gradient:"from-purple-500  to-pink-600"
+    },
+    {
+      id:4,
+      size:"w-80 h-80",
+      position:"bottom-1/3 right-1/4",
+      gradient:"from-yellow-500  to-cyan-600"
+    }
+  ]
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FloatingShapes
